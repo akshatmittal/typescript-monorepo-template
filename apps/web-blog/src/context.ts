0f8cc0a0380ccf2ac8d81b89771df04f2fe4cd0f
Akshat Mittal <1667481+akshatmittal@users.noreply.github.com>
2026-05-15 20:25:03 +0000

    Monorepo

---
+++ apps/web-blog/src/context.ts
@@ -0,0 +1,13 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export interface RouterContext {
+  queryClient: QueryClient;
+}
+
+export function getContext(): RouterContext {
+  const queryClient = new QueryClient();
+
+  return {
+    queryClient,
+  };
+}
---
+++ apps/web-blog/vite.config.ts
@@ -0,0 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const config = defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    tailwindcss(),
+    tanstackStart({
+      prerender: {
+        enabled: true,
+        autoStaticPathsDiscovery: true,
+        autoSubfolderIndex: true,
+      },
+    }),
+    viteReact(),
+  ],
+});
+
+export default config;
